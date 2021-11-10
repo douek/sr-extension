74570efbdd9a8e275d8614c8db5671d067d472ce
Daniel Douek <dadouek@gmail.com>
2021-11-10 13:34:38 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Use of this source code is governed by a BSD-style license that can be1
 // found in the LICENSE file.
 import qs from 'qs'
 
